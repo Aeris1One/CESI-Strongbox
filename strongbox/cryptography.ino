dd0f8314c3836a44f5f6068fccbb3fd5387b5c87
Charles Pingard <charles.pingard@viacesi.fr>
2022-11-07 11:03:19 +0100

    🔐 cryptography: implement modexp function

---
+++ strongbox/cryptography.ino
@@ -0,0 +1,28 @@
+/*
+  modexp
+  Input : The base a, the exposant e and n
+  Output: The remainder of the euclidean division of a^e by n
+  Effect: Modular exponentiation
+*/
+
+// Why is "e" signed ? ¯\_(ツ)_/¯
+// It can't be negative but meh, there's a check in the logigram so let's do it like this !
+
+uint16_t modexp(uint16_t a, int16_t e, uint16_t n) {
+  // r is signed in order to be able to return -1 in case the impossible is true
+  int16_t r;
+  if (e < 0) {
+    Serial.println("Authentication error : e was negative !");
+    return (-1);
+  }
+  if (a == 0 || n == 1) {
+    r = 0;
+  } else {
+    r = 1;
+    while (e != 0) {
+      r = (r * (a % n)) % n;
+      e = e - 1;
+    }
+  }
+  return r;
+}
